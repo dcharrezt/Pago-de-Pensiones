08edb6dedc357a0f7627663fe4d146198b6b5c0f
Luis Enrique Murillo Vizcardo <luisito.luis.16.lm@gmail.com>
2015-07-18 01:23:19 -0500

    aprendiendo jquery opinen

--- Interface/logica.js
+++ Interface/logica.js
@@ -1,29 +1,11 @@
-function verificar(x){
-	switch(x){
-		case 1:
-			return "inicio";
-			break;
-		case 2:
-			return "pago";
-			break;
-		case 3:
-			return "actualizar";
-			break;
-		case 4:
-			return "constancias";
-			break;
-		case 5:
-			return "logout";
-			break;
-	}
-}
-function cambiar(x){
-	var c;
-	c=verificar(x);
-	document.getElementById(c).style.backgroundColor="#757575";
-}
-function normal(x){
-	var c;
-	c=verificar(x);
-	document.getElementById(c).style.backgroundColor="#5D5D5D";
-}
+$(document).ready(function() {
+	$('.Botones').mouseenter(function() {
+	   $(this).css("background-color", "#757575");
+	});
+	$('.Botones').mouseleave(function() {
+	   $(this).css("background-color", "#5D5D5D");
+	});
+	$('#inicio').click(function(){
+		$('#PaginaInicio').slideDown(5000);
+	});
+});
