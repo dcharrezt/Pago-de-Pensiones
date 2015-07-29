b1aaafa0b25eb867b61bd49110bdb3da506206f9
edsonlipa <edson.lipa@gmail.com>
2015-07-29 18:43:08 -0500

    Constacias
    sera con numero de dni

--- Interface/logica.js
+++ Interface/logica.js
@@ -9,14 +9,23 @@ $(document).ready(function() {
 		$('#PaginaInicio').slideDown(4000);
 		$('#PaginaPago').hide();
 		$('#PaginaActualizar').hide();
+		$('#PaginaConstancias').hide();
 	});
 	$('#pago').click(function(){
 		$('#PaginaPago').fadeIn();
 		$('#PaginaInicio').hide();
 		$('#PaginaActualizar').hide();
+		$('#PaginaConstancias').hide();
 	});
 	$('#actualizar').click(function(){
 		$('#PaginaActualizar').fadeIn();
+		$('#PaginaInicio').hide();
+		$('#PaginaPago').hide();
+		$('#PaginaConstancias').hide();
+	});
+	$('#constancias').click(function(){	
+		$('#PaginaConstancias').fadeIn();
+		$('#PaginaActualizar').hide();
 		$('#PaginaInicio').hide();
 		$('#PaginaPago').hide();
 	});
