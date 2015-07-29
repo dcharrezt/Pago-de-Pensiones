c15fe6d14bcdc824c68fc8aab5563930178845ee
Luis Enrique Murillo Vizcardo <luisito.luis.16.lm@gmail.com>
2015-07-29 00:02:44 -0500

    poniendo el formulario de pensiones

--- Interface/existe.php
+++ Interface/existe.php
@@ -11,6 +11,7 @@
 			<h3>Nombre:</h3>
 			<?php
 				$mensaje=$_GET['mensaje']; //variable pasada desde busqueda.php
+				$valor=@$_GET['var'];
 				echo "<p>$mensaje</p>";
 			?>
 			<div class="boton" id="Pagar"><span>Pagar Pension</span></div>
@@ -18,9 +19,36 @@
 		</div>
 		<div id="Paga">
 			<h3>PAGAR</h3>
-			<!-- ************************************************************** -->
-			<!-- ********AQUI IRA EL FORMULARIO DE PAGA DE PENSION************* -->
-			<!-- ************************************************************** -->
+			<?php
+				$valor=$_GET['var'];
+				echo "<p>$valor</p>";
+			?>
+			<h4>Carrera:
+						<select>
+							<option value="-----">- - - - -
+							<option value="ComputerScience">Ciencia de la computacion
+							<option value="Medicina">Medicina
+							<option value="Civil">Civil
+						</select>
+			</h4></p>
+			<h4>Mes a pagar:
+						<select>
+							<option value="-----">- - - - -
+							<option value="enero">Enero
+							<option value="febrero">Febrero
+							<option value="marzo">Marzo
+							<option value="abrir">Abrir
+							<option value="mayo">Mayo
+							<option value="junio">Junio
+							<option value="julio">Julio
+							<option value="agosto">Agosto
+							<option value="setiembre">Setiembre
+							<option value="octubre">Octubre
+							<option value="noviembre">Noviembre
+							<option value="diciembre">Diciembre
+						</select>
+					</h4></p>
+			<button ty="Submit">ENVIAR</button>
 		</div>
 	</body>
 </html>
